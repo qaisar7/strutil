799640b385f16a214e49da166df859b9575859d6
Qaisar - Ubuntu <qaisar.sardar@gmail.com>
2017-04-05 14:12:36 +1000

    Adding errors to Reverse()

--- strutil.go
+++ strutil.go
@@ -1,13 +1,19 @@
 // Package stringutil contains utility functions for working with strings
 package strutil
 
+import (
+	"fmt"
+)
 
-func Reverse(s1 string) string {
+func Reverse(s1 string) (string, error) {
+	if s1 == "" {
+		return "", fmt.Errorf("strutil/Reverse(): the provided string is empty")
+	}
 	l := len(s1)
 	s2 := make([]rune, l)
-	j:=0
-	for i:=len(s1)-1; i>=0 && j< len(s1); i,j = i-1, j+1 {
+	j := 0
+	for i := len(s1) - 1; i >= 0 && j < len(s1); i, j = i-1, j+1 {
 		s2[i] = rune(s1[j])
 	}
-	return string(s2)
+	return string(s2), nil
 }
--- strutil_test.go
+++ strutil_test.go
@@ -16,9 +16,20 @@ func TestXxx(t *testing.T) {
 			input: "Hey",
 			want:  "yeH",
 		},
+		{
+			name:    "fail, empty string",
+			input:   "",
+			wantErr: true,
+		},
 	}
 	for _, test := range tests {
-		got := Reverse(test.input)
+		got, err := Reverse(test.input)
+		if (err != nil) != test.wantErr {
+			t.Errorf("want error:%v, got:%v", test.wantErr, err)
+		}
+		if err != nil {
+			continue
+		}
 		if got != test.want {
 			t.Errorf("Test(%s): got:%s, want:%s", test.name, got, test.want)
 		}
