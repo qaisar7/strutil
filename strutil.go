1cb4c525e1fb276b70354b60129488628c5fc4f2
Qaisar - Ubuntu <qaisar.sardar@gmail.com>
2017-04-06 17:31:12 +1000

    Added CamelCase function and test

--- strutil.go
+++ strutil.go
@@ -3,8 +3,15 @@ package strutil
 
 import (
 	"fmt"
+	"strings"
 )
 
+const (
+	space rune = rune(' ')
+)
+
+// Reverse reverses a given string.
+// Hello -> olleH
 func Reverse(s1 string) (string, error) {
 	if s1 == "" {
 		return "", fmt.Errorf("strutil/Reverse(): the provided string is empty")
@@ -16,4 +23,27 @@ func Reverse(s1 string) (string, error) {
 		s2[i] = rune(s1[j])
 	}
 	return string(s2), nil
+}
+
+// CamelCase changes a given string to camelCase by removing spaces and capitalizing the next rune after space.
+// 'hey mate' -> heyMate
+func CamelCase(s string) string {
+	s2 := []rune{}
+	c := false
+	for _, r := range s {
+		if r == space {
+			c = true
+			continue
+		}
+		if c {
+			r = rune(strings.ToUpper(string(r))[0])
+			c = false
+		}
+		s2 = append(s2, r)
+	}
+	return string(s2)
+}
+
+func main() {
+	fmt.Println(CamelCase("hey mate"))
 }
--- strutil_test.go
+++ strutil_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestXxx(t *testing.T) {
+func TestReverse(t *testing.T) {
 	tests := []struct {
 		name    string
 		input   string
@@ -32,6 +32,36 @@ func TestXxx(t *testing.T) {
 		}
 		if got != test.want {
 			t.Errorf("Test(%s): got:%s, want:%s", test.name, got, test.want)
+		}
+	}
+}
+
+func TestCamelCase(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  string
+	}{
+		{
+			name:  "hey mate",
+			input: "hey mate",
+			want:  "heyMate",
+		},
+		{
+			name:  "extra characters",
+			input: "what's up mate?",
+			want:  "what'sUpMate?",
+		},
+		{
+			name:  "simple",
+			input: "wow",
+			want:  "wow",
+		},
+	}
+	for _, test := range tests {
+		got := CamelCase(test.input)
+		if got != test.want {
+			t.Errorf("Test(%s): got:%s., want:%s. %v %d", test.name, got, test.want)
 		}
 	}
 }
